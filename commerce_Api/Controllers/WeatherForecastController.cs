e20da00eddb7bfe0d6eba9d9fe7ab72fe5bcffba
臣 梁 <2985733771@qq.com>
2021-03-22 16:25:11 +0800

    添加项目文件。

---
+++ commerce_Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using commerce_Api.MODEL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace commerce_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        StudentContent stuContent;
+        /// <summary>
+        /// 依赖注入
+        /// </summary>
+        /// <param name="stuContent"></param>
+        public WeatherForecastController(StudentContent studentContent)
+        {
+           this.stuContent = studentContent;
+        }
+      
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> GetStudentList()
+        {
+            return stuContent.students.ToList();
+        }
+    }
+}
---
+++ commerce_Api/MODEL/StudentContent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace commerce_Api.MODEL
+{
+    public class StudentContent : DbContext
+    {
+        public StudentContent() { }
+        public StudentContent(DbContextOptions<StudentContent> options) : base(options) { }
+
+        public DbSet<Student> students { get; set; }
+    }
+}
